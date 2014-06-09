db2eb31f30b24acc9ff0c2f37ec8565e6c597540
John van Weel <jvweel@tricode.nl>
2014-06-09 21:31:10 +0200

    Added gradient test to debugger

--- adalight/adalight-node/src/main/java/nl/johnvanweel/iot/light/debugger/DebugLightsPanel.java
+++ adalight/adalight-node/src/main/java/nl/johnvanweel/iot/light/debugger/DebugLightsPanel.java
@@ -13,7 +13,55 @@ public class DebugLightsPanel extends JPanel implements ILightService {
 	private Color[] colorsBuffer = new Color[20];
 	private Graphics g;
 
+	private Color[] gradientTest = new Color[100];
 
+	JSlider sliderRed = new JSlider(0, 128, 127);
+	JSlider sliderGreen = new JSlider(0, 128, 127);
+	JSlider sliderBlue = new JSlider(0, 128, 127);
+	JSlider sliderR = new JSlider(0, 10, 1);
+	JSlider sliderG = new JSlider(0, 10, 1);
+	JSlider sliderB = new JSlider(0, 10, 1);
+
+
+	public DebugLightsPanel() {
+		sliderRed.setMajorTickSpacing(10);
+		sliderRed.setMinorTickSpacing(1);
+		add(sliderRed);
+		sliderGreen.setMajorTickSpacing(10);
+		sliderGreen.setMinorTickSpacing(1);
+		add(sliderGreen);
+		sliderBlue.setMajorTickSpacing(10);
+		sliderBlue.setMinorTickSpacing(1);
+		add(sliderBlue);
+		sliderR.setMajorTickSpacing(10);
+		sliderR.setMinorTickSpacing(1);
+		add(sliderR);
+		sliderG.setMajorTickSpacing(10);
+		sliderG.setMinorTickSpacing(1);
+		add(sliderG);
+		sliderB.setMajorTickSpacing(10);
+		sliderB.setMinorTickSpacing(1);
+		add(sliderB);
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+
+
+				while (true) {
+					double frequency = ((double) sliderR.getValue()) / 10D;
+
+					for (int i = 0; i < 100; i++) {
+						int red = (int) (Math.sin((frequency) * i + sliderR.getValue()) * sliderRed.getValue() + 128);
+						int green = (int) (Math.sin((frequency)* i + sliderG.getValue()) * sliderGreen.getValue() + 128);
+						int blue = (int) (Math.sin((frequency)* i + sliderB.getValue()) * sliderBlue.getValue() + 128);
+
+						gradientTest[i] = new Color(red, green, blue);
+					}
+				}
+			}
+		}).start();
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
@@ -23,6 +71,13 @@ public class DebugLightsPanel extends JPanel implements ILightService {
 			if (colors[i] != null) {
 				g.setColor(colors[i]);
 				g.fillOval(28 * i + 20, 100, 25, 25);
+			}
+		}
+
+		for (int i = 0; i < gradientTest.length; i++) {
+			if (gradientTest[i] != null) {
+				g.setColor(gradientTest[i]);
+				g.fillRect(200 + (3 * i), 100, 3, 250);
 			}
 		}
 	}
