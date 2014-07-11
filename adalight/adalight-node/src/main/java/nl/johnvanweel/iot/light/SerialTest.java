a5c03e9c4ecae8f9ab140de952fb890c82ff4f82
John van Weel <jvweel@tricode.nl>
2014-07-11 21:21:59 +0200

    Libboblight no longer has to be started before the application

---
+++ adalight/adalight-node/src/main/java/nl/johnvanweel/iot/light/SerialTest.java
@@ -0,0 +1,19 @@
+package nl.johnvanweel.iot.light;
+
+/**
+ *
+ */
+public class SerialTest {
+	public static void main(String[] args) throws Throwable {
+//		XBee xBee = new XBee();
+//		xBee.open("/dev/ttyUSB0", 9600);
+//		xBee.addPacketListener(response -> {
+//			ZNetRxIoSampleResponse response1 = (ZNetRxIoSampleResponse) response;
+//			System.out.println("==========================================================");
+//			System.out.println(response1.getAnalog0());
+//			System.out.println(response1.getAnalog1());
+//			System.out.println("==========================================================");
+//
+//		});
+	}
+}
--- adalight/adalight-node/src/main/java/nl/johnvanweel/iot/light/access/boblight/BobILightService.java
+++ adalight/adalight-node/src/main/java/nl/johnvanweel/iot/light/access/boblight/BobILightService.java
@@ -3,6 +3,7 @@ package nl.johnvanweel.iot.light.access.boblight;
 import com.sun.jna.Pointer;
 import nl.johnvanweel.iot.light.service.ILightService;
 import nl.johnvanweel.iot.light.service.LightException;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -10,40 +11,57 @@ import org.springframework.beans.factory.annotation.Autowired;
  * Implementation using the boblight daemon running on the default address.
  */
 public class BobILightService implements ILightService {
-    public static final int ALL_PIXELS = -1;
+	private final Logger log = Logger.getLogger(BobILightService.class);
 
-    private BoblightDaemon daemon;
-    private Pointer bob;
+	public static final int ALL_PIXELS = -1;
 
-    @Autowired
-    public BobILightService(final BoblightDeamonFactory factory) {
+	private BoblightDaemon daemon;
+	private Pointer bob;
 
-        this.daemon = factory.getInstance();
+	@Autowired
+	public BobILightService(final BoblightDeamonFactory factory) {
+		this.daemon = factory.getInstance();
 
-        bob = daemon.boblight_init();
-        int resultCode = daemon.boblight_connect(bob, null, -1, 5000000);
-        int prioResult = daemon.boblight_setpriority(bob, 128);
+		new Thread(() -> {
+			boolean connected = false;
 
-        if (resultCode == 0 || prioResult == 0) {
-            throw new LightException("Cannot connect to boblightd.");
-        }
-    }
+			while (!connected) {
+				bob = daemon.boblight_init();
+				int resultCode = daemon.boblight_connect(bob, null, -1, 5000000);
+				int prioResult = daemon.boblight_setpriority(bob, 128);
 
-    @Override
-    public void setPixel(int pixelNumber, int red, int green, int blue) {
-        if (pixelNumber >= 0) {
-            daemon.boblight_addpixel(bob, pixelNumber, new int[]{red, green, blue});
-        }
-    }
+				if (resultCode == 0 || prioResult == 0) {
+					log.warn("Cannot connect to boblight daemon. Retrying in 1000ms.");
 
-    @Override
-    public void allPixels(int red, int green, int blue) {
-        daemon.boblight_addpixel(bob, ALL_PIXELS, new int[]{red, green, blue});
-        send();
-    }
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+					}
+				} else {
+					log.info("Connection to boblight daemon established.");
+					connected = true;
+				}
+			}
+		}).start();
 
-    @Override
-    public void send() {
-        daemon.boblight_sendrgb(bob, 1, new int[]{255});
-    }
+		log.debug("Started connection Thread.");
+	}
+
+	@Override
+	public void setPixel(int pixelNumber, int red, int green, int blue) {
+		if (pixelNumber >= 0) {
+			daemon.boblight_addpixel(bob, pixelNumber, new int[]{red, green, blue});
+		}
+	}
+
+	@Override
+	public void allPixels(int red, int green, int blue) {
+		daemon.boblight_addpixel(bob, ALL_PIXELS, new int[]{red, green, blue});
+		send();
+	}
+
+	@Override
+	public void send() {
+		daemon.boblight_sendrgb(bob, 1, new int[]{255});
+	}
 }
